5e592cbbb9162f020e60f466dd5836fd660a9dd3
Hiroshi Hatake <cosmo0920.oucc@gmail.com>
2019-06-24 18:45:16 +0900

    Stop mixed message variable usage

--- lib/winevt/subscribe.rb
+++ lib/winevt/subscribe.rb
@@ -4,9 +4,9 @@ module Winevt
       alias_method :each_raw, :each
       def each
         each_raw do |xml, message, string_inserts|
-          message = message.gsub(/(%\d+)/, '\1$s')
-          message = sprintf(message, *string_inserts) rescue message.gsub(/(%\d+)/, "?")
-          yield(xml, message, string_inserts)
+          placeholdered_message = message.gsub(/(%\d+)/, '\1$s')
+          replaced_message = sprintf(placeholdered_message, *string_inserts) rescue message.gsub(/(%\d+)/, "?")
+          yield(xml, replaced_message, string_inserts)
         end
       end
     end
