03fe6a6b952553569ae1153b78c3c779b5de0ad0
Hiroshi Hatake <hatake@clear-code.com>
2019-09-24 18:09:36 +0900

    Format with clang-format

--- ext/winevt/winevt_bookmark.c
+++ ext/winevt/winevt_bookmark.c
@@ -1,18 +1,22 @@
 #include <winevt_c.h>
 
-static void bookmark_free(void *ptr);
+static void
+bookmark_free(void* ptr);
 
-static const rb_data_type_t rb_winevt_bookmark_type = {
-  "winevt/bookmark", {
-    0, bookmark_free, 0,
-  }, NULL, NULL,
-  RUBY_TYPED_FREE_IMMEDIATELY
-};
+static const rb_data_type_t rb_winevt_bookmark_type = { "winevt/bookmark",
+                                                        {
+                                                          0,
+                                                          bookmark_free,
+                                                          0,
+                                                        },
+                                                        NULL,
+                                                        NULL,
+                                                        RUBY_TYPED_FREE_IMMEDIATELY };
 
 static void
-bookmark_free(void *ptr)
+bookmark_free(void* ptr)
 {
-  struct WinevtBookmark *winevtBookmark = (struct WinevtBookmark *)ptr;
+  struct WinevtBookmark* winevtBookmark = (struct WinevtBookmark*)ptr;
   if (winevtBookmark->bookmark)
     EvtClose(winevtBookmark->bookmark);
 
@@ -23,23 +27,22 @@ static VALUE
 rb_winevt_bookmark_alloc(VALUE klass)
 {
   VALUE obj;
-  struct WinevtBookmark *winevtBookmark;
-  obj = TypedData_Make_Struct(klass,
-                              struct WinevtBookmark,
-                              &rb_winevt_bookmark_type,
-                              winevtBookmark);
+  struct WinevtBookmark* winevtBookmark;
+  obj = TypedData_Make_Struct(
+    klass, struct WinevtBookmark, &rb_winevt_bookmark_type, winevtBookmark);
   return obj;
 }
 
 static VALUE
-rb_winevt_bookmark_initialize(int argc, VALUE *argv, VALUE self)
+rb_winevt_bookmark_initialize(int argc, VALUE* argv, VALUE self)
 {
   PWSTR bookmarkXml;
   VALUE wbookmarkXmlBuf;
   DWORD len;
-  struct WinevtBookmark *winevtBookmark;
+  struct WinevtBookmark* winevtBookmark;
 
-  TypedData_Get_Struct(self, struct WinevtBookmark, &rb_winevt_bookmark_type, winevtBookmark);
+  TypedData_Get_Struct(
+    self, struct WinevtBookmark, &rb_winevt_bookmark_type, winevtBookmark);
 
   if (argc == 0) {
     winevtBookmark->bookmark = EvtCreateBookmark(NULL);
@@ -49,9 +52,15 @@ rb_winevt_bookmark_initialize(int argc, VALUE *argv, VALUE self)
     Check_Type(rb_bookmarkXml, T_STRING);
 
     // bookmarkXml : To wide char
-    len = MultiByteToWideChar(CP_UTF8, 0, RSTRING_PTR(rb_bookmarkXml), RSTRING_LEN(rb_bookmarkXml), NULL, 0);
-    bookmarkXml = ALLOCV_N(WCHAR, wbookmarkXmlBuf, len+1);
-    MultiByteToWideChar(CP_UTF8, 0, RSTRING_PTR(rb_bookmarkXml), RSTRING_LEN(rb_bookmarkXml), bookmarkXml, len);
+    len = MultiByteToWideChar(
+      CP_UTF8, 0, RSTRING_PTR(rb_bookmarkXml), RSTRING_LEN(rb_bookmarkXml), NULL, 0);
+    bookmarkXml = ALLOCV_N(WCHAR, wbookmarkXmlBuf, len + 1);
+    MultiByteToWideChar(CP_UTF8,
+                        0,
+                        RSTRING_PTR(rb_bookmarkXml),
+                        RSTRING_LEN(rb_bookmarkXml),
+                        bookmarkXml,
+                        len);
     bookmarkXml[len] = L'\0';
     winevtBookmark->bookmark = EvtCreateBookmark(bookmarkXml);
     ALLOCV_END(wbookmarkXmlBuf);
@@ -63,14 +72,15 @@ rb_winevt_bookmark_initialize(int argc, VALUE *argv, VALUE self)
 static VALUE
 rb_winevt_bookmark_update(VALUE self, VALUE event)
 {
-  struct WinevtQuery *winevtQuery;
-  struct WinevtBookmark *winevtBookmark;
+  struct WinevtQuery* winevtQuery;
+  struct WinevtBookmark* winevtBookmark;
 
   winevtQuery = EventQuery(event);
 
-  TypedData_Get_Struct(self, struct WinevtBookmark, &rb_winevt_bookmark_type, winevtBookmark);
+  TypedData_Get_Struct(
+    self, struct WinevtBookmark, &rb_winevt_bookmark_type, winevtBookmark);
 
-  if(EvtUpdateBookmark(winevtBookmark->bookmark, winevtQuery->event))
+  if (EvtUpdateBookmark(winevtBookmark->bookmark, winevtQuery->event))
     return Qtrue;
 
   return Qfalse;
@@ -80,10 +90,11 @@ static VALUE
 rb_winevt_bookmark_render(VALUE self)
 {
   WCHAR* wResult;
-  struct WinevtBookmark *winevtBookmark;
+  struct WinevtBookmark* winevtBookmark;
   VALUE utf8str;
 
-  TypedData_Get_Struct(self, struct WinevtBookmark, &rb_winevt_bookmark_type, winevtBookmark);
+  TypedData_Get_Struct(
+    self, struct WinevtBookmark, &rb_winevt_bookmark_type, winevtBookmark);
   wResult = render_event(winevtBookmark->bookmark, EvtRenderBookmark);
   utf8str = wstr_to_rb_str(CP_UTF8, wResult, -1);
 
@@ -93,7 +104,8 @@ rb_winevt_bookmark_render(VALUE self)
   return utf8str;
 }
 
-void Init_winevt_bookmark(VALUE rb_cEventLog)
+void
+Init_winevt_bookmark(VALUE rb_cEventLog)
 {
   rb_cBookmark = rb_define_class_under(rb_cEventLog, "Bookmark", rb_cObject);
 
