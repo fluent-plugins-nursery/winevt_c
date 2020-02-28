8df4e237b9bcecda96884af704dadefbbbc1f901
Hiroshi Hatake <hatake@clear-code.com>
2020-02-28 17:06:49 +0900

    Write out more detailed description for example code

--- ext/winevt/winevt_channel.c
+++ ext/winevt/winevt_channel.c
@@ -9,7 +9,8 @@
  *  require 'winevt'
  *  channels = []
  *  @channel = Winevt::EventLog::Channel.new
- *  # If users want to retrieve all channel names including Debug and Analytical,
+ *  # If users want to retrieve all channel names that
+ *  # including type of Debug and Analytical,
  *  # it should be set as true.
  *  @channel.force_enumerate = false
  *  @channel.each do |channel|
